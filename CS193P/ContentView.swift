e46a180972309decea71af6570d7e02738561234
Egor Kraus <kraus.egor@gmail.com>
2026-03-01 00:58:40 +0300

    Test update

--- CS193P/ContentView.swift
+++ CS193P/ContentView.swift
@@ -7,55 +7,90 @@
 
 import SwiftUI
 import SwiftData
+import CoreData
+import AVFoundation
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
+    let emojis = ["😈", "💀", "🎃", "👽", "👹", "😈", "💀", "👻", "👽", "👹", "😈", "💀", "🎃", "👽", "👹"]
+    @State var cardCount: Int = 4
+    
+    
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
+        VStack {
+            ScrollView {
+                cards
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
+            Spacer()
+            cardCountAjuster
         }
+        .padding()
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount/*emojis.indices*/, id: \.self) { index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+        }
+        .foregroundColor(.orange)
+    }
+    
+    var cardCountAjuster: some View {
+        HStack {
+            cardRevocer
+            Spacer()
+            cartAdder
+        }
+        .imageScale(.small)
+        .padding()
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+        
+    var cardRevocer: some View {
+      return cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill").font(.largeTitle)
+    }
+    
+    var cartAdder: some View {
+       cardCountAdjuster(by: +1, symbol: "rectangle.stack.badge.plus.fill").font(.largeTitle)
+        }
+    
+    
     }
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
+struct CardView: View {
+    let content: String
+    @State var isFaceUp: Bool = true
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 23)
+            Group {
+                base.fill(.regularMaterial)
+                base.strokeBorder(lineWidth: 2)
+               Text(content)
+                    .font(.largeTitle)
+                    
             }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        //    isFaceUp = !isFaceUp
         }
     }
 }
-
 #Preview {
     ContentView()
         .modelContainer(for: Item.self, inMemory: true)
 }
+
+
